3ff912fa521cf6fe405f8e9739dd8cb8dece9f77
Bubblecrown <suchanya.namwong1@gmail.com>
2023-02-23 03:40:37 +0700

    edit header

--- src/pages/AboutUs/Member.tsx
+++ src/pages/AboutUs/Member.tsx
@@ -7,6 +7,7 @@ import { Container, Header, HeaderBar, Section } from "./AboutStyle";
 import {
   CardDetail,
   CardMember,
+  CardMemberBlock,
   CardMemberBody,
   CardMemberContainer,
   CardSection,
@@ -23,41 +24,44 @@ const Member = () => {
             <HeaderMember logoColor="#000000" />
           </Header>
         </HeaderBar>
+
         <CardMemberContainer>
-          <CardMemberBody>
-            <CardSection>
-              <CardMember
-                c="#fff"
-                animate={{}}
-                transition={{
-                  type: "spring",
-                  stiffness: 50,
-                  damping: 6,
-                }}
-              ></CardMember>
-            </CardSection>
-            <CardDetail>
-              <MemberDetailHeader>
-                <H3Roboto d="block">Responsibility</H3Roboto>
-                <H1Noto d="block">Pimchawisa SapaRram</H1Noto>
-                <Responsibilities>
-                  <H3Eng>Ux/Ui Designer</H3Eng>
-                  <H3Eng>Art Director</H3Eng>
-                  <H3Eng>3D Modeller</H3Eng>
-                  <H3Eng>Animator</H3Eng>
-                </Responsibilities>
-              </MemberDetailHeader>
-              <BodyEng indent="2rem">
-                is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived
-                not only five centuries, but also the leap into electronic
-                typesetting, remaining essentially unchanged. It was popularised
-                in the 1960s
-              </BodyEng>
-            </CardDetail>
-          </CardMemberBody>
+          <CardMemberBlock>
+            <CardMemberBody>
+              <CardSection>
+                <CardMember
+                  c="#fff"
+                  animate={{}}
+                  transition={{
+                    type: "spring",
+                    stiffness: 50,
+                    damping: 6,
+                  }}
+                ></CardMember>
+              </CardSection>
+              <CardDetail>
+                <MemberDetailHeader>
+                  <H3Roboto d="block">Responsibility</H3Roboto>
+                  <H1Noto d="block">Pimchawisa SapaRram</H1Noto>
+                  <Responsibilities>
+                    <H3Eng>Ux/Ui Designer</H3Eng>
+                    <H3Eng>Art Director</H3Eng>
+                    <H3Eng>3D Modeller</H3Eng>
+                    <H3Eng>Animator</H3Eng>
+                  </Responsibilities>
+                </MemberDetailHeader>
+                <BodyEng indent="2rem">
+                  is simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry's standard dummy text ever
+                  since the 1500s, when an unknown printer took a galley of type
+                  and scrambled it to make a type specimen book. It has survived
+                  not only five centuries, but also the leap into electronic
+                  typesetting, remaining essentially unchanged. It was
+                  popularised in the 1960s
+                </BodyEng>
+              </CardDetail>
+            </CardMemberBody>
+          </CardMemberBlock>
         </CardMemberContainer>
       </Container>
     </Section>
--- src/pages/AboutUs/MemberStyle.ts
+++ src/pages/AboutUs/MemberStyle.ts
@@ -3,33 +3,47 @@ import { Card } from "../../components/card/CardStyle";
 
 export const CardMemberContainer = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+export const CardMemberBlock = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 60%;
 `;
 export const CardMemberBody = styled.div`
   width: 70%;
   border: 2px #fff solid;
   display: flex;
   gap: 3rem;
+  height: 100%;
 
-  @media only screen and (max-width: 850px) {
+  @media only screen and (max-width: 1150px) {
+    width: 100%;
     flex-wrap: wrap;
   }
 `;
-export const CardMember = styled(Card)`
-max-width: 100%;
-`;
 export const CardSection = styled.div`
   border: 2px red solid;
-  flex-grow: 0.4;
+  flex-grow: 0.1;
   max-width: 100%;
-  flex-basis: 100%;
+  flex-basis: 0;
+  align-items: center;
+  display: flex;
 `;
+export const CardMember = styled(Card)`
+  max-width: 100%;
+  min-width: 50px;
+`;
+
 export const CardDetail = styled.div`
-  flex-basis: 100%;
-  flex-grow: 0.6;
+  flex-basis: 0;
+  flex-grow: 0.9;
   display: flex;
   flex-direction: column;
   justify-content: center;
