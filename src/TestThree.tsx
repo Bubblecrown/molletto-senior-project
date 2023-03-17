27ec7ad24c3247abf461b5b97779aa2285b55951
Bubblecrown <suchanya.namwong1@gmail.com>
2023-03-18 01:01:09 +0700

    test fog

--- src/TestThree.tsx
+++ src/TestThree.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { Canvas, extend, useFrame, useThree } from "@react-three/fiber";
-import { Mesh } from "three";
+import { Mesh, Vector3 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import "./interfaces/OrbitControls";
 extend({ OrbitControls });
@@ -22,9 +22,28 @@ const Box = () => {
     }
   });
   return (
-    <mesh ref={ref}>
+    <mesh ref={ref} castShadow receiveShadow>
       <boxBufferGeometry />
-      <meshStandardMaterial color="hotpink" />
+      <meshPhysicalMaterial color="hotpink" />
+    </mesh>
+  );
+};
+
+const Floor = (props: any) => {
+  return (
+    <mesh {...props} receiveShadow>
+      <boxBufferGeometry args={[10, 1, 10]} />
+      <meshPhysicalMaterial />
+    </mesh>
+  );
+};
+
+const Bulb = (props: any) => {
+  return (
+    <mesh {...props}>
+      <pointLight castShadow />
+      <sphereBufferGeometry />
+      <meshPhongMaterial emissive="yellow" />
     </mesh>
   );
 };
@@ -32,11 +51,19 @@ const Box = () => {
 const TestThree = () => {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas style={{ background: "#fff2f4" }}>
-        <pointLight position={[10, 10, 10]} />
+      <Canvas
+        style={{ background: "#fff2f4" }}
+        camera={{ position: [3, 3, 3] }}
+        shadows
+      >
+        {/* <pointLight position={[10, 10, 10]} /> */}
         <Orbit />
+        <ambientLight intensity={0.2}/>
+        <fog attach='fog' args={['white', 1, 8]}/>
         <Box />
+        <Bulb position={[0, 2, 0]} />
         <axesHelper args={[5]} />
+        <Floor position={[0, -1.5, 0]} />
       </Canvas>
     </div>
   );
